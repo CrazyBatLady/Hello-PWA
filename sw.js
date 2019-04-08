c81818b77558365844bf7f8c556904f1b2b6f8a7
CrazyBatLady <48226790+CrazyBatLady@users.noreply.github.com>
2019-04-08 18:40:09 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,21 +10,27 @@ var filesToCache =[
 /* Start the service worker and cache all of the app's content */
 this.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-        console.log('[ServiceWorker] Caching app shell');
+    this.caches.open(cacheName).then(function(cache) {
+        this.console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 this.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+  this.console.log('[ServiceWorker] Activate');
 });
 
 /* Serve cached content when offline */
 this.addEventListener('fetch', function(e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    this.caches.match(e.request).then(function(response) {
+      return response || this.fetch(e.request);
     })
   );
+});
+
+
+
+this.addEventListener('fetch', function(event) {
+ this.console.log(event.request.url);
 });
